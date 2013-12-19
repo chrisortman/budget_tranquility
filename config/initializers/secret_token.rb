c498a0d6d47fa8a9f378d6926186ca1ca51411cb
Chris Ortman <chrisortman@gmail.com>
2013-12-18 18:57:38 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BudgetTranquility::Application.config.secret_key_base = '1151eabf921526cd71feae9434e3053dc7bee7d993edde7f478c8bb58af01d9507e3dca6a763e35a5fdb8eac423ba19afac350932cfd38d144e605c063640e9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BudgetTranquility::Application.config.session_store :cookie_store, key: '_budget_tranquility_session'
